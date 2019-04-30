3d666d59a8e9b4d01fe00c7d25f5c4ee81874d09
Sinan Karateke <s.karateke@gmx.de>
2019-04-30 14:12:33 +0000

    header.php und footer.php eingefügt

--- index.php
+++ index.php
@@ -1,27 +1,4 @@
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <title>Selling modul</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <link rel="stylesheet" href="/css/style.css" type="text/css" />
-
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
-  <!--image picker-->
-  <link rel="stylesheet" href="/image-picker-master/image-picker/image-picker.css" type="text/css" />
-  <script src="/image-picker-master/image-picker/image-picker.js"></script>
-  
-</head>
-<body>
-
-<div class="jumbotron text-center">
-  <div class="enjoy-css"><b>TerraTectum</b></div>
-  <div class="fun-with-css">Erdauswahlmodul für das TerraTectum "Shopsystem"</div> 
-</div>
+<?php require_once "header.php"?>
   
 <div class="container text-center">
     <!--Auswahl  --> 
@@ -438,5 +415,4 @@ $("#test").click(function(){
 
 <?php require_once("table.php");?>
 
-</body>
-</html>
+<?php require_once("footer.php");?>
